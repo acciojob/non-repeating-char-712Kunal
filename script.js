c1f4639a0ab6c099ec17a7dea2a432b7e40e178d
Kunal Jadhav <155985737+712Kunal@users.noreply.github.com>
2026-08-19 00:23:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let arr={};
+
+	for(let i=0;i<str.length();i++){
+		arr[str.charAt(i)-'a']++;
+	}
+
+	for (let index = 0; index < arr.length; index++) {
+		if(arr[index]===1){
+			return str.charAt(index+'a');
+		}
+	}
+
+	return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
